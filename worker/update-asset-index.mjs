cc82b9aca9d497832182dcbe355c33c1bbb26cac
Sebastian Hartte <shartte@users.noreply.github.com>
2024-06-16 17:02:16 +0200

    Now updated the right script...

--- worker/update-asset-index.mjs
+++ worker/update-asset-index.mjs
@@ -1,44 +1,51 @@
 export default {
   async scheduled(event, env, ctx) {
     const bucket = env.GUIDE_ASSETS;
-
-    // List directories in root
-    const response = await bucket.list({
+    const listOptions = {
       prefix: "",
       delimiter: "/",
-    });
+    };
 
     const versions = [];
+    async function processPrefixes(delimitedPrefixes) {
+      for (const prefix of delimitedPrefixes) {
+        const devVersion = prefix === "development/";
 
-    for (const prefix of response.delimitedPrefixes) {
-      const devVersion = prefix === "development/";
+        if (!prefix.match(/^minecraft-([^/]+)\/$/) && !devVersion) {
+          continue;
+        }
+        const indexFile = prefix + "index.json";
 
-      if (!prefix.match(/^minecraft-([^/]+)\/$/) && !devVersion) {
-        continue;
+        const indexResponse = await bucket.get(indexFile);
+        if (!indexResponse) {
+          console.warn("Index-File %s does not exist.", indexFile);
+          continue;
+        }
+
+        try {
+          const versionIndexContent = await indexResponse.json();
+          console.info("Content of %s: %o", indexFile, versionIndexContent);
+          versions.push({
+            format: versionIndexContent.format,
+            generated: versionIndexContent.generated,
+            gameVersion: versionIndexContent.gameVersion,
+            modVersion: versionIndexContent.modVersion,
+            // This is where the actual data lives in V1 guides
+            url: "https://guide-assets.appliedenergistics.org/" + indexFile,
+            development: devVersion,
+          });
+        } catch (e) {
+          console.error("Failed to process index file %s", indexFile);
+        }
       }
-      const indexFile = prefix + "index.json";
+    }
 
-      const indexResponse = await bucket.get(indexFile);
-      if (!indexResponse) {
-        console.warn("Index-File %s does not exist.", indexFile);
-        continue;
-      }
-
-      try {
-        const versionIndexContent = await indexResponse.json();
-        console.info("Content of %s: %o", indexFile, versionIndexContent);
-        versions.push({
-          format: versionIndexContent.format,
-          generated: versionIndexContent.generated,
-          gameVersion: versionIndexContent.gameVersion,
-          modVersion: versionIndexContent.modVersion,
-          // This is where the actual build-data lives in V1 guides
-          url: "https://guide-assets.appliedenergistics.org/" + indexFile,
-          development: devVersion,
-        });
-      } catch (e) {
-        console.error("Failed to process index file %s", indexFile);
-      }
+    // List directories in root
+    let response = await bucket.list(listOptions);
+    await processPrefixes(response.delimitedPrefixes);
+    while (response.truncated) {
+      response = await bucket.list({ ...listOptions, cursor: response.cursor });
+      await processPrefixes(response.delimitedPrefixes);
     }
 
     const indexJsonContent = JSON.stringify({
